1fe219443680a9e54ac4ed04840ce829dc7bdc8f
jotavede <jvictordagostini@gmail.com>
2026-05-20 19:45:17 -0300

    questão9

--- Uni6Exe06.java
+++ Uni6Exe06.java
@@ -1,5 +1,14 @@
+/*
+Faça um programa que leia um valor N inteiro.
+Com base neste valor, crie um vetor do tipo real.
+Em seguida, solicite ao usuário informar essa quantidade N de valores reais para popular o vetor.
+Após ter preenchido o vetor, solicite que o usuário informe um outro valor real.
+Informe para o usuário se este valor informado se encontra cadastrado no vetor.
+*/
+
 public class Uni6Exe06 {
     public static void main(String[] args) {
+    
         
     }
 }
---
+++ Uni6Exe09.java
@@ -0,0 +1,21 @@
+/*
+Um cinema pretende fazer uma pesquisa para avaliar o grau de satisfação de seus clientes.
+Trinta deles foram ouvidos e para cada um perguntou-se o sexo (1=feminino 2=masculino), uma nota para o cinema (zero até dez, valor inteiro) e a idade.
+
+Baseado nisto faça um programa que informe:
+
+qual a nota média recebida pelo cinema;
+qual a nota média atribuída pelos homens;
+qual a nota atribuída pela mulher mais jovem;
+quantas das mulheres com mais de 50 anos deram nota superior a média recebida pelo cinema.
+
+Utilize os conceitos aprendidos sobre vetores (ou mesmo o uso de matriz).
+
+*/
+
+public class Uni6Exe09 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
